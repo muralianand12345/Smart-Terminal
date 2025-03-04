38dd1a23113816f24098f1e867b3046d81efa51f
Murali Anand <smurali1607@gmail.com>
2025-03-04 15:54:12 +0530

    Enhance package metadata: add long description to setup.py, include LICENSE in setup.cfg, and update .gitignore to exclude dist directory

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="smart-terminal",
     version="1.1.0",
     description="AI-Powered Terminal Command Generator and Executor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Murali Anand",
     author_email="smurali1607@gmail.com",
     url="https://github.com/muralianand12345/Smart-Terminal",
@@ -20,6 +25,7 @@ setup(
     ],
     python_requires=">=3.9",
     license="MIT",
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
